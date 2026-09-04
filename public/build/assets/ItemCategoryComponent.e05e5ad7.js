6cb8afa579fe84bbed51aea82d5c5802ff080c66
Leoivard Omondi <leoivardomondi@gmail.com>
2026-09-04 23:58:32 +0300

    Remove duplicate branch selector from dashboard body, keeping single top header branch selector

--- public/build/assets/ItemCategoryComponent.e05e5ad7.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,f as t,o as n,l as r}from"./app.727fbfb2.js";const s={name:"ItemCategoryComponent"};function a(c,p,_,m,f,i){const e=t("router-view");return n(),r(e)}const l=o(s,[["render",a]]);export{l as default};
